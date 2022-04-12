1f067a675118ed0199e7ed9d1387cf912f46f28c
LGatodu47 <62804774+LGatodu47@users.noreply.github.com>
2022-04-12 20:20:13 +0200

    Initial commit

---
+++ src/main/java/fr/lgatodu47/meowrapper/Arguments.java
@@ -0,0 +1,126 @@
+package fr.lgatodu47.meowrapper;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static fr.lgatodu47.meowrapper.Logger.*;
+
+/**
+ * Utility class that parses String arrays into arguments and vice-versa.
+ * The {@link Arguments#argumentMap} should remain unmodifiable.
+ */
+public class Arguments {
+    private final Map<String, String> argumentMap;
+
+    public Arguments(Map<String, String> argumentMap) {
+        this.argumentMap = new HashMap<>(argumentMap);
+    }
+
+    public Arguments(String[] args) {
+        this.argumentMap = new HashMap<>();
+        for(int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if(arg.startsWith("--")) { // first argument scheme
+                String key = arg.substring(2);
+                String value = null;
+                try {
+                    value = args[i + 1];
+                    if(!value.startsWith("--")) i++; // if it isn't a single argument we skip the value of this argument
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+
+                argumentMap.put(key, value);
+            } else if(arg.startsWith("-")) { // second argument scheme
+                String key;
+                String value;
+                if(arg.contains("=")) {
+                    key = arg.substring(1, arg.indexOf('='));
+                    value = arg.substring(arg.indexOf('=') + 1);
+
+                    if(value.isEmpty()) {
+                        error("Found invalid argument '%s' at index %d", arg, i);
+                        continue;
+                    }
+                } else {
+                    key = arg.substring(1);
+                    value = null;
+                }
+
+                if(key.isEmpty()) {
+                    error("Found invalid argument '%s' at index %d", arg, i);
+                    continue;
+                }
+
+                argumentMap.put(key, value);
+            } else { // no argument
+                error("Found invalid argument '%s' at index %d", arg, i);
+            }
+        }
+    }
+
+    /**
+     * Gets a value from the argument map.
+     *
+     * @param key The corresponding key of the value to get.
+     * @return An {@link Optional} containing the value if it exists.
+     */
+    public Optional<String> get(String key) {
+        return Optional.ofNullable(argumentMap.get(key));
+    }
+
+    /**
+     * Gets a path from the argument map.
+     *
+     * @param key The corresponding key of the path to get.
+     * @param orElse The path to return if the value is absent.
+     * @return The found path if it exists, otherwise the specified path.
+     */
+    public Path getPath(String key, Path orElse) {
+        return get(key).map(Paths::get).orElse(orElse);
+    }
+
+    /**
+     * @param argument The name of the argument.
+     * @return {@code true} if the specified argument is present in this map (if it was specified, value may be {@code null}).
+     */
+    public boolean contains(String argument) {
+        return argumentMap.containsKey(argument);
+    }
+
+    /**
+     * Parses the stored argument map into a String array. <strong>This method omits keys mapped to {@code null} values.</strong><br>
+     * Here is an example of an expected output: {@code ["--arg", "value", "--argument", "value"]}
+     * @return A String array containing the parsed arguments.
+     */
+    public String[] toNonnullArgs() {
+        String[] result = new String[(int) argumentMap.values().stream().filter(Objects::nonNull).count() * 2]; // we only want non-null entries
+        List<Map.Entry<String, String>> list = argumentMap.entrySet().stream().filter(entry -> entry.getValue() != null).toList();
+
+        for(int i = 0; i < list.size(); i++) {
+            Map.Entry<String, String> entry = list.get(i);
+            result[i * 2] = "--".concat(entry.getKey());
+            result[i * 2 + 1] = entry.getValue();
+        }
+
+        return result;
+    }
+
+    /**
+     * Parses the stored argument map into a String array. <strong>This method doesn't omit keys mapped to {@code null} values.</strong><br>
+     * Here is an example of an expected output: {@code ["--arg", "value", "--null_arg", "--argument", "value"]}
+     * @return A String array containing the parsed arguments.
+     */
+    public String[] toArgs() {
+        String[] result = new String[argumentMap.values().stream().mapToInt(s -> s == null ? 1 : 2).sum()]; // Allocate 2 slots when a value exists, otherwise 1
+        List<Map.Entry<String, String>> list = new ArrayList<>(argumentMap.entrySet());
+
+        for(int i = 0, j = 0; i < list.size(); i++) {
+            Map.Entry<String, String> entry = list.get(i);
+            result[j++] = "--".concat(entry.getKey());
+            if(entry.getValue() != null) result[j++] = entry.getValue();
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/fr/lgatodu47/meowrapper/MeoWrapper.java
@@ -0,0 +1,110 @@
+package fr.lgatodu47.meowrapper;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+import static fr.lgatodu47.meowrapper.Logger.*;
+
+public class MeoWrapper {
+    public static final Path RUNNING_PATH = tryDetectRunPath();
+
+    private void start(Arguments args) {
+        Optional<RuntimeEnvironment> optEnvironment = args.get("environment").flatMap(RuntimeEnvironment::byName);
+        if(optEnvironment.isEmpty()) {
+            error("Missing or invalid 'environment' argument! Type 'help' for more info about the needed arguments");
+            return;
+        }
+
+        RuntimeEnvironment env = optEnvironment.get();
+
+        Optional<String> optVersion = args.get("version");
+        if(optVersion.isEmpty()) {
+            error("Missing 'version' argument! Type 'help-<%s>' to have the list of arguments for the current environment", env.getName().toLowerCase(Locale.ROOT));
+            return;
+        }
+
+        String version = optVersion.get();
+
+        DEBUG = args.contains("debug");
+
+        if(!env.preSetup()) return;
+        if(env.setup(args, version)) return;
+
+        launch(env);
+    }
+
+    private void launch(RuntimeEnvironment env) {
+        if(!env.preLaunch()) return;
+
+        info("Launching Minecraft on '%s' with main '%s' and with args '%s'!", env.getName(), env.getMainClassName(), Arrays.toString(env.getLaunchArguments()));
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+
+        Class<?> clazz;
+        try {
+            clazz = loader.loadClass(env.getMainClassName());
+        } catch (ClassNotFoundException e) {
+            error("No class '%s' has been found in classpath!", env.getMainClassName());
+            e.printStackTrace();
+            return;
+        }
+
+        Method method;
+        try {
+            method = clazz.getMethod("main", String[].class);
+        } catch (NoSuchMethodException e) {
+            error("No main method found in class '%s'!", env.getMainClassName());
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            method.invoke(null, (Object) env.getLaunchArguments());
+        } catch (IllegalAccessException e) {
+            error("Can't access main method in class '%s'!", env.getMainClassName());
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            error("Caught an exception when launching game!");
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean logHelp(String firstArg) {
+        if(firstArg.contains("help") || "?".equals(firstArg)) {
+            if(firstArg.contains("help-")) {
+                String envString = firstArg.substring(firstArg.indexOf('-') + 1);
+                RuntimeEnvironment.byName(envString).ifPresentOrElse(RuntimeEnvironment::logArgumentsList, () -> error("Could not find environment with name '%s'!", envString));
+            } else {
+                info("Welcome in MeoWrapper! To run the program, you will need to specify arguments depending on the running environment you want. There are currently 3 run environments: Client, Server and Data. To learn more about the required arguments of the environment of your choice, use the argument 'help-<env>'!");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static Path tryDetectRunPath() {
+        try {
+            Path runningPath = Paths.get(MeoWrapper.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            while (!Files.isDirectory(runningPath)) {
+                runningPath = runningPath.getParent();
+            }
+            return runningPath;
+        } catch (Exception e) {
+            error("Failed to locate running dir! Try move the jar in another place");
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        if(args == null || args.length == 0) throw new NullPointerException("No arguments specified! Type 'help' for more info about the needed arguments!");
+        if(logHelp(args[0])) return;
+        new MeoWrapper().start(new Arguments(args));
+    }
+}
